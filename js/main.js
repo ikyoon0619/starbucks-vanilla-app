facca10fbadd0c814da9e4562cdaa1238820a4ae
allegory_one <ikyoon0619@gmail.com>
2021-05-24 17:11:57 +0900

    HTML,JS: 헤더의 badge의 스크롤 에니메이션 추가

--- js/main.js
+++ js/main.js
@@ -15,3 +15,27 @@ searchInputEl.addEventListener("blur", () => {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // 배지숨기기
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      //배지보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간)
